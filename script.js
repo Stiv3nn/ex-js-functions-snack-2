bee2b48224bc1686a5b768cc9cdef0cf693515b2
Stiven hoxha <stivenhoxha007@gmail.com>
2025-04-22 17:54:20 +0200

    snack 1 2 3

---
+++ script.js
@@ -0,0 +1,44 @@
+// 🏆 SNACK 1
+// Crea una funzione che somma due numeri.
+
+// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+// function somma(a, b) {
+//     return a + b;
+// }
+
+// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+// const somma = function(a, b) {
+//     return a + b;
+// }
+
+// Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+// const somma = () => a + b; // RISULTATO FINALE DEL SNACK 1
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// 🏆 SNACK 2
+// Crea una arrow function che calcola il quadrato di un numero.
+
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+// const quadrato = n => n * n;
+// const quadrato = n => n ** 2; (MODO ALTERNATIVO NEL JAVASCRIPT MODERNO ---> n elevato al quadrato)
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// 🏆 SNACK 3
+// Crea una funzione eseguiOperazione
+
+// Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+// const somma = (a, b) => a + b;
+// const moltiplica = (a, b) => a + b;
+
+
+// const eseguiOperazioni = (a, b, operatore) => {
+//     operatore(a, b);
+// }
+
+// eseguiOperazioni(2, 3, somma);
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
